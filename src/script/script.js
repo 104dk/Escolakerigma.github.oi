24b8eb6af156fa3bb28fc2b97b4c51e91e37bfa5
104dk <maianderson104@gmail.com>
2024-11-14 11:49:00 -0300

    subindo site pro netlify
    1 envio dos arquivos.
    2 publicar no netlify

---
+++ src/script/script.js
@@ -0,0 +1,7 @@
+// Selecionando o botão
+const btnClick = document.getElementById('btnClick');
+
+// Função para executar quando o botão for clicado
+btnClick.addEventListener('click', () => {
+    alert('Você clicou no botão!');
+});
